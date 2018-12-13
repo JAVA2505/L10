62007040aba7ed42d88ada9b9e4d24ad8d3e91be
JAVA2505 <carelcontrols@ukr.net>
2018-12-13 21:45:45 +0200

    file reading implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File f = new File("aaa.txt");
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+        System.out.println(f.getAbsolutePath());
+        InputStream is = new FileInputStream(f);
+        int b;
+        while ((b = is.read()) != -1) {
+            System.out.print((char) b);
+        }
+        is.close();
     }
 }
